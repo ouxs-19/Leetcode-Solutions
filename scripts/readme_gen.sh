1257a34ea020864e4f5f72b5189537fbbfbe730a
ouxs <shawnmurphyy1@gmail.com>
2023-09-13 02:48:11 +0100

    fixed regex in bash script from any to 1 or more

--- scripts/readme_gen.sh
+++ scripts/readme_gen.sh
@@ -2,7 +2,7 @@
 
 files=`echo "$1" | tr "," "\n"`
 
-challenges=$(echo $files | cut -d '/' -f 1 | uniq | awk '/^[0-9]*\.\s/ {print}')
+challenges=$(echo $files | cut -d '/' -f 1 | uniq | awk '/^[0-9]+\.\s/ {print}')
 
 echo $challenges | while read challenge; do echo $challenge && python3 $GEN_MD "$challenge"; done
 
