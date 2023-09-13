c427a565699e3de038bbbd551f2d6cda7e826fae
ouxs <shawnmurphyy1@gmail.com>
2023-09-13 02:58:27 +0100

    added condition for empty lines in bash script

--- scripts/readme_gen.sh
+++ scripts/readme_gen.sh
@@ -4,6 +4,6 @@ files=`echo "$1" | tr "," "\n"`
 
 challenges=$(echo $files | cut -d '/' -f 1 | uniq | awk '/^[0-9]+\.\s/ {print}')
 
-echo $challenges | while read challenge; do echo $challenge && python3 $GEN_MD "$challenge"; done
+echo $challenges | while read challenge; do echo $challenge && ! [ -z "$challenge" ]  && python3 $GEN_MD "$challenge"; done
 
         
